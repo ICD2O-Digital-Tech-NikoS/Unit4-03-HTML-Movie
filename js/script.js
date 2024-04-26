82a330e169a5721d28d9391ca7d8b86ae32ae5b0
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-04-26 15:08:37 +0000

    u

--- js/script.js
+++ js/script.js
@@ -9,14 +9,21 @@ function ageLimit() {
   // get user input and declare it as a variable for their age
   let userAge = document.getElementById("age").value;
   // if statement to see if user age is above certain age limits for different movie ratings
-  if (userAge >= 17) {
+  if (userAge >= 100) {
+    document.getElementById("user-info").innerHTML = "Bruv you are a dinosaur, good luck getting out of that chair my boi. L R.I.P";
+  } 
+  else if (userAge >= 17) {
     document.getElementById("user-info").innerHTML = "You can see an R rated movie alone.";
-  } else if (userAge >= 13) {
+  } 
+  else if (userAge >= 13) {
     document.getElementById("user-info").innerHTML = "You can see a PG-13 rated movie alone.";
-  } else if (userAge >= 5) {
+  } 
+  else if (userAge >= 5) {
     document.getElementById("user-info").innerHTML = "You can see a G or PG rated movie alone.";
-  } else {
-    document.getElementById("user-info").innerHTML = "buddy, your younger than  years old, go play with toys or something.";
+  } 
+  else {
+    document.getElementById("user-info").innerHTML = "Buddy, your younger than 5 years old, go play with toys or something.";
   }
-  document.getElementById("user-info").innerHTML = "Thanks for verifying your age!";
+ // adds another message at the end of each message.
+ document.getElementById("user-info").innerHTML = document.getElementById("user-info").innerHTML + "<br><br>Thanks for verifying your age!";
 }
